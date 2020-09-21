61f82700e1a2516ec22207b0a21ecd8ab61bd1bb
aika-hashi <yu_akiramenai542_0224@yahoo.co.jp>
2020-09-21 15:35:34 +0900

    rails_12factor

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,19 @@
 class ApplicationController < ActionController::Base
   # before_action :authenticate_user!
+  # before_action :basic_auth
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname,:email,:password,:password_confirmation,:firstname, :familyname, :firstname_kana, :familyname_kana, :date,])
+  end
+
+  # private
+
+  # def basic_auth
+  #   authenticate_or_request_with_http_basic do |username, password|
+  #     username == '' && password == '2222'
+  #   end
   end
 end
