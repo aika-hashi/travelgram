e3c69ae650c3d87dc18b0af0c5647078471aa5f9
aika-hashi <yu_akiramenai542_0224@yahoo.co.jp>
2020-09-21 14:38:35 +0900

    バリデーション追加

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -6,4 +6,7 @@ class Trip < ApplicationRecord
   belongs_to_active_hash :crowd
   belongs_to_active_hash :day
   
+
+  validates :area_id, numericality: { other_than: 1 }
+  validates :image,:local, :title, :price,:discount,:spot_text,presence: true
 end
