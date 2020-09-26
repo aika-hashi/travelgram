10b0f54ae6949dbb93f4df36b9018c04c0c08375
aika-hashi <yu_akiramenai542_0224@yahoo.co.jp>
2020-09-26 12:25:21 +0900

    ブックマークテーブル作成

---
+++ db/migrate/20200926032033_create_bookmarks.rb
@@ -0,0 +1,12 @@
+class CreateBookmarks < ActiveRecord::Migration[6.0]
+  def change
+    create_table :bookmarks do |t|
+      t.references :user, null: false, foreign_key: true, null: false
+
+      t.references :tweet, null: false, foreign_key: true, null: false
+
+
+      t.timestamps
+    end
+  end
+end
